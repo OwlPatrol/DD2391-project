e737398c9bb761afd15d40a323fc221fd61a312c
Mathias Magnusson <mathias@magnusson.space>
2024-09-20 14:23:08 +0200

    Initilalize vulnerable application

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # DD2391-project
+
+## Vulnerable App
+
+Located in `vulnerable-app/`. Install Apache maven (tested on version 3.9.6),
+navigate to the app's directory and run the command `mvn compile exec:java -q`.
+
+It is a web server that listen on port 8080 and logs the current time, the
+connecting clients' IP address and user agent and the requests method and path,
+all of which are pretty common for web servers to log.
+
+By default it only listens for connections coming from the host itself. To
+listen for connections coming from other hosts (which is pretty dangerous), append `-Dexec.args="listen-any"` to the command mentioned above.
---
+++ vulnerable-app/src/main/java/vulnerableApp/App.java
@@ -0,0 +1,50 @@
+package vulnerableApp;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+public class App implements HttpHandler {
+    private static final Logger logger = LogManager.getLogger(App.class);
+
+    public static void main(String[] args) throws IOException {
+        var listenAny = args.length > 0 && args[0].equals("listen-any");
+
+        HttpServer server = HttpServer.create(
+            listenAny ?
+                new InetSocketAddress(8080) :
+                new InetSocketAddress("127.0.0.1", 8080),
+            0
+        );
+
+        server.createContext("/", new App());
+
+        server.setExecutor(null);
+        server.start();
+
+        logger.info("Started server on http://" + (listenAny ? "0.0.0.0" : "127.0.0.1") + ":8080");
+    }
+
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        var method = exchange.getRequestMethod();
+        var path = exchange.getRequestURI().getPath();
+        var clientIp = exchange.getRemoteAddress().getAddress().getHostAddress();
+        var userAgent = exchange.getRequestHeaders().getFirst("User-Agent");
+        logger.info(method + " " + path + " " + clientIp + " " + userAgent);
+
+        var response = "Hello, this a the HTTP server!";
+        exchange.sendResponseHeaders(200, response.getBytes().length);
+        var os = exchange.getResponseBody();
+        os.write(response.getBytes());
+        os.close();
+    }
+}
+
